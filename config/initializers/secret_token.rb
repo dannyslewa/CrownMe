acf1604db5d4216b648aa1335949172609037849
Danny Slewa <dannyslewa@gmail.com>
2015-02-24 17:08:37 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrownMe::Application.config.secret_key_base = '591e92938854c0d5874b4ca05bfc63c71e073d3dd0a46c54458ab8623382fec7a5803bff6b7fa0e78c5bd70d8447273632673ba5cd49d1742d8535eaa5a16e18'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrownMe::Application.config.session_store :cookie_store, key: '_CrownMe_session'
